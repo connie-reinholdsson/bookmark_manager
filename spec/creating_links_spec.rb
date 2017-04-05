7ece7c24efcaececca68d82b927c132c5b1eea53
Connie Reinholdsson <conniereinholdsson@Connies-MacBook-Pro.local>
2017-04-05 11:31:50 +0100

    Added DatabaseCleaner

--- spec/creating_links_spec.rb
+++ spec/creating_links_spec.rb
@@ -3,7 +3,7 @@ feature 'submit link' do
   scenario 'form to submit new link' do
     visit '/links/new'
     fill_in(:title, with: 'connie')
-    fill_in(:link, with: 'www.connie.com')
+    fill_in(:url, with: 'www.connie.com')
     click_button 'Submit'
 
     expect(current_path).to eq '/links'
